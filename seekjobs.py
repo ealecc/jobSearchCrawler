e0e3597aa02a81b240b5646ffb2290c1b6a89d77
Edmund Creevy <ealecc621@outlook.com>
2021-03-10 00:50:42 +1000

    Produce text table output from seek

--- seekjobs.py
+++ seekjobs.py
@@ -1,32 +1,48 @@
 # A beautifulsoup job search webscraper
 from bs4 import BeautifulSoup
 from requests import get
+import texttable
 
 url = "https://www.seek.com.au/system-administrator-jobs/in-Toowoomba-&-Darling-Downs-QLD"
 soup = BeautifulSoup(get(url).text, 'html.parser')
 
 jobs = soup.findAll('article')
 
-def find_attribute (object, tag, data):
-    try:
-        out = object.find(tag,{"data-automation":data}).text
-    except AttributeError:
-        return "Null"
-    except:
-        return "!!!Unknown error!!!"
-    
-    return out
+table = texttable.Texttable(max_width=200)
 
 # Find and print Seek listings
 for job in jobs:
     pageurl = "https://www.seek.com.au/" + job.find('a',{"data-automation":"jobTitle"}).get('href')
+    page = BeautifulSoup(get(pageurl).text, 'html.parser')
     result = []
 
-    result.append(find_attribute(job, 'a', "jobTitle"))
-    result.append(find_attribute(job, 'a', "jobCompany"))
-    result.append(find_attribute(job, 'a', "jobLocation"))
-    result.append(find_attribute(job, 'a', "jobClassification"))
-    result.append(find_attribute(job, 'span', "subClassification"))
-    result.append(find_attribute(job, 'span', "jobShortDescription").replace('\n',''))
+    title = job.find('a',{"data-automation":"jobTitle"}).text
+    result.append(title)
 
-    print("\t".join(result))
+    try:
+        company = job.find('a',{"data-automation":"jobCompany"}).text
+    except AttributeError:
+        company = "Private Advertisor"
+    result.append(company)
+
+    location = job.find('a',{"data-automation":"jobLocation"}).text
+    result.append(location)
+
+    try:
+        salary = job.find('span',{"data-automation":"jobSalary"}).span.text
+    except:
+        salary = "No Listed Salary"
+    result.append(salary)
+
+    description = job.find('span',{"data-automation":"jobShortDescription"}).span.text
+    result.append(description)
+
+    listed_date = job.find('span',{"data-automation":"jobListingDate"}).text
+    result.append(listed_date)
+    
+    start_date = page.find('dd',{"data-automation":"job-detail-date"}).span.span.text
+    result.append(start_date)
+
+    table.add_row(result)
+
+print(table.draw())
