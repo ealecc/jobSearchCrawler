dabf8842ebb6f50773835717ed66fe30f152bd4c
Edmund Creevy <ealecc621@outlook.com>
2021-03-12 18:30:14 +1000

    wrap seek search in function

--- seekjobs.py
+++ seekjobs.py
@@ -1,48 +1,48 @@
 # A beautifulsoup job search webscraper
 from bs4 import BeautifulSoup
 from requests import get
-import texttable
 
-url = "https://www.seek.com.au/system-administrator-jobs/in-Toowoomba-&-Darling-Downs-QLD"
-soup = BeautifulSoup(get(url).text, 'html.parser')
+URL = "https://www.seek.com.au/system-administrator-jobs/in-Toowoomba-&-Darling-Downs-QLD"
 
-jobs = soup.findAll('article')
+# Find jobs on seek and return a list of
+def seek_jobs (search_query):
+    output = ["Title","Location","Salary","Description","Date listed","Start Date"]
 
-table = texttable.Texttable(max_width=200)
-
-# Find and print Seek listings
-for job in jobs:
-    pageurl = "https://www.seek.com.au/" + job.find('a',{"data-automation":"jobTitle"}).get('href')
-    page = BeautifulSoup(get(pageurl).text, 'html.parser')
-    result = []
-
-    title = job.find('a',{"data-automation":"jobTitle"}).text
-    result.append(title)
-
-    try:
-        company = job.find('a',{"data-automation":"jobCompany"}).text
-    except AttributeError:
-        company = "Private Advertisor"
-    result.append(company)
-
-    location = job.find('a',{"data-automation":"jobLocation"}).text
-    result.append(location)
-
-    try:
-        salary = job.find('span',{"data-automation":"jobSalary"}).span.text
-    except:
-        salary = "No Listed Salary"
-    result.append(salary)
-
-    description = job.find('span',{"data-automation":"jobShortDescription"}).span.text
-    result.append(description)
-
-    listed_date = job.find('span',{"data-automation":"jobListingDate"}).text
-    result.append(listed_date)
+    soup = BeautifulSoup(get(search_query).text, 'html.parser')
+    jobs = soup.findAll('article')
     
-    start_date = page.find('dd',{"data-automation":"job-detail-date"}).span.span.text
-    result.append(start_date)
+    for job in jobs:
+        pageurl = "https://www.seek.com.au/" + job.find('a',{"data-automation":"jobTitle"}).get('href')
+        page = BeautifulSoup(get(pageurl).text, 'html.parser')
+        job_info = []
 
-    table.add_row(result)
+        title = job.find('a',{"data-automation":"jobTitle"}).text
+        job_info.append(title)
 
-print(table.draw())
+        try:
+            company = job.find('a',{"data-automation":"jobCompany"}).text
+        except AttributeError:
+            company = "Private Advertisor"
+        job_info.append(company)
+
+        location = job.find('a',{"data-automation":"jobLocation"}).text
+        job_info.append(location)
+
+        try:
+            salary = job.find('span',{"data-automation":"jobSalary"}).span.text
+        except:
+            salary = "No Listed Salary"
+        job_info.append(salary)
+
+        description = job.find('span',{"data-automation":"jobShortDescription"}).span.text
+        job_info.append(description)
+
+        listed_date = job.find('span',{"data-automation":"jobListingDate"}).text
+        job_info.append(listed_date)
+        
+        start_date = page.find('dd',{"data-automation":"job-detail-date"}).span.span.text
+        job_info.append(start_date)
+
+        output.append(job_info)
+    
+    return output
